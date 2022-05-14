7e620fde07856cfbc524543b3e45b7e21ba59017
ro-ed <robinedin94@gmail.com>
2022-05-14 11:27:00 +0200

    PWA

--- DateApp/src/app/app.module.ts
+++ DateApp/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuModule } from './menu/menu.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -17,7 +19,13 @@ import { MenuModule } from './menu/menu.module';
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
-    MenuModule
+    MenuModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
     
   ],
     
