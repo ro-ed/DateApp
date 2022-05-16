fae1917a768e45d2bcd3bf31c5961f3d84eca4e0
ro-ed <robinedin94@gmail.com>
2022-05-16 15:34:53 +0200

    Update main.ts

--- DateApp/src/main.ts
+++ DateApp/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { defineCustomElements } from '@ionic/pwa-elements/loader'
 
-
+defineCustomElements(window);
 
 if (environment.production) {
   enableProdMode();
@@ -14,4 +14,4 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
 
-  defineCustomElements(window);
+ 
